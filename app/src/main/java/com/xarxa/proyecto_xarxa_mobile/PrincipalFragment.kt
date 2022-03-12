6560fa67f1d11bc21a1e79eeca6edd43ea558a6b
juandcs99 <jd.cabrera@iesdoctorbalmis.com>
2022-03-12 19:32:59 +0100

    creación nueva activity, para separar login con acciones, navigationDrawer aplicado a la AccionesActivity

--- app/src/main/java/com/xarxa/proyecto_xarxa_mobile/PrincipalFragment.kt
+++ app/src/main/java/com/xarxa/proyecto_xarxa_mobile/PrincipalFragment.kt
@@ -1,32 +1,16 @@
 package com.xarxa.proyecto_xarxa_mobile
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.res.ResourcesCompat
-import androidx.core.graphics.blue
-import androidx.core.graphics.green
-import androidx.core.graphics.red
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomappbar.BottomAppBar
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.math.MathUtils
-import com.google.android.material.navigation.NavigationView
 import com.xarxa.proyecto_xarxa_mobile.databinding.LayoutPrincipalBinding
 
 class PrincipalFragment : Fragment() {
 
     private lateinit var _binding: LayoutPrincipalBinding
     private val binding get() = _binding
-    private lateinit var toggle: ActionBarDrawerToggle
-    private lateinit var navigationView: NavigationView
-    private lateinit var bottomAppBar: BottomAppBar
-    private lateinit var screen: FrameLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,52 +20,8 @@ class PrincipalFragment : Fragment() {
         super.onCreateView(inflater, container, savedInstanceState)
         _binding = LayoutPrincipalBinding.inflate(inflater, container, false)
         val view = binding.root
-        (activity as AppCompatActivity).setSupportActionBar(binding.bottomAppBar)
 
-        navigationView = binding.navigationView
-        bottomAppBar = binding.bottomAppBar
-        screen = binding.screen
-
-        val bottomSheetBehavior = BottomSheetBehavior.from(navigationView)
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
-
-        bottomAppBar.setNavigationOnClickListener {
-            if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_HIDDEN) {
-                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-            } else {
-                bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
-            }
-        }
-
-        navigationView.setNavigationItemSelectedListener { menuItem ->
-            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
-            true
-        }
-
-        screen.setOnClickListener {
-            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
-        }
-
-        bottomSheetBehavior.addBottomSheetCallback(object :
-            BottomSheetBehavior.BottomSheetCallback() {
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                val baseColor = Color.BLACK
-                // 60% opacity
-                val baseAlpha =
-                    ResourcesCompat.getFloat(resources, R.dimen.material_emphasis_medium)
-                // Map slideOffset from [-1.0, 1.0] to [0.0, 1.0]
-                val offset = (slideOffset - (-1f)) / (1f - (-1f)) * (1f - 0f) + 0f
-                val alpha = MathUtils.lerp(0f, 255f, offset * baseAlpha).toInt()
-                val color = Color.argb(alpha, baseColor.red, baseColor.green, baseColor.blue)
-                screen.setBackgroundColor(color)
-            }
-
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
-            }
-        })
 
         return view
     }
-
-
 }
